6dfbcb0e65c97355533ddcad66b8ea88a7022ce5
xieminhui <xieminhui@corp.netease.com>
2020-01-02 17:06:52 +0800

    + tuple.py

---
+++ src/test demo/tuple.py
@@ -0,0 +1,17 @@
+"""
+@Date: 2020-01-02 17:00:36
+@LastEditors: xieminhui
+@LastEditTime: 2020-01-02 17:02:51
+"""
+tuple = ("tom", "jan", "liny")
+
+# 获取
+
+print(f"tuple第一位是{tuple[0]}")
+
+# 删除 只允许删除整个元组
+
+del tuple
+
+print(f"tuple删除后是{tuple}")
+
