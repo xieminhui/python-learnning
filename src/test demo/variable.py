d273f0aa3ee4410c589939fe74d10b568aa15a34
xieminhui <xieminhui@corp.netease.com>
2020-01-02 11:15:21 +0800

    + variable.py

---
+++ src/test demo/variable.py
@@ -0,0 +1,26 @@
+"""
+@Date: 2020-01-02 10:49:01
+@LastEditors: xieminhui
+@LastEditTime: 2020-01-02 10:49:28
+"""
+
+
+"""
+ptyhon variable
+
+"""
+
+num = 1
+float = 1.1
+str = "2"
+print(num, float, str)
+
+list = [1, 2, 3, 4]
+list[3] = 5
+print(list)
+
+# 元组不能赋值
+tuple = (1, 2, 3)
+# tuple[1] = 3 error
+print(tuple)
+
