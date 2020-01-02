1ba6c442994c5239b47ffbbe8002169f7d2d061b
xieminhui <xieminhui@corp.netease.com>
2020-01-02 11:35:05 +0800

    + operator.py

---
+++ src/test demo/operator.py
@@ -0,0 +1,30 @@
+"""
+@Date: 2020-01-02 11:17:14
+@LastEditors: xieminhui
+@LastEditTime: 2020-01-02 11:17:23
+"""
+
+
+# Operator
+
+a = 1
+b = 2
+c = 3
+
+# add
+
+print(f"{a} + {b} = {a + b}")
+
+# Subtraction
+print(f"{a} - {b} = {a - b}")
+
+# multiplication
+
+print(f"{a} * {b} = {a * b}")
+
+# division
+
+print(f"{a} / {b} = {a / b}")
+
+### more
+print("....")
