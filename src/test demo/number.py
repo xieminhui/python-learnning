0071d790a6731102ac8fe77358e012fa268c91d3
xieminhui <xieminhui@corp.netease.com>
2020-01-02 14:27:07 +0800

    + number.py

---
+++ src/test demo/number.py
@@ -0,0 +1,29 @@
+"""
+@Date: 2020-01-02 11:40:12
+@LastEditors: xieminhui
+@LastEditTime: 2020-01-02 11:40:19
+"""
+import math
+
+var1 = 1
+var2 = 10
+
+print(f"num: {var1, var2}")
+
+# 十六进制
+
+print("十六进制显示")
+print(f"{hex(var1)}")
+
+
+# 固定常量
+
+print(f"π:{math.pi}")
+
+
+# 数学函数
+x = -10
+num1 = abs(x)
+
+print(f"{x}的绝对值是{num1}")
+
